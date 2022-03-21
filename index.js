624a3d19719a26d67f61fdd8efb9106a239ad20b
webdevterri <32005382+webdevterri@users.noreply.github.com>
2022-03-20 20:43:16 -0500

    Coded JS to generate 4 random passwords

--- index.js
+++ index.js
@@ -74,27 +74,43 @@ const characters = [
   `M`,
 ];
 
-let password1 = []
-let password2 = []
-let password3 = []
-let password4 = []
 
-// document.getElementById("btn-generate").addEventListener("click", {
-//   // function to generate random passwords, iterate 4 times and place each in #pass[i + 1]
-// });
+
+document.getElementById("btn-generate").addEventListener("click", generatePasswords);
 
 function generatePasswords() {
+  let password1 = []
+  let password2 = []
+  let password3 = []
+  let password4 = []
   // iterate the following 4 times
-  for (let i = 0; i < 15; i++){
-    // get a random index from characters [0 - 72]
-    // push to array of password#
-    // join array .join('')
-    const n = Math.floor(Math.random() * 73)
-    password1.push(characters[n])
+    for (let i = 0; i < 15; i++){
+      const n = Math.floor(Math.random() * 73)
+      password1.push(characters[n])
+    }
+    document.getElementById(`pass1`).innerHTML = `<p class="password-text">${password1.join('')}</p>`
+    console.log(password1)
 
-    document.getElementById('pass1').innerHTML = `<p class="password-text">${password1.join('')}</p>`
-  }
-  console.log(password1)
+    for (let i = 0; i < 15; i++){
+      const n = Math.floor(Math.random() * 73)
+      password2.push(characters[n])
+    }
+    document.getElementById(`pass2`).innerHTML = `<p class="password-text">${password2.join('')}</p>`
+    console.log(password2)
+
+    for (let i = 0; i < 15; i++){
+      const n = Math.floor(Math.random() * 73)
+      password3.push(characters[n])
+    }
+    document.getElementById(`pass3`).innerHTML = `<p class="password-text">${password3.join('')}</p>`
+    console.log(password3)
+
+    for (let i = 0; i < 15; i++){
+      const n = Math.floor(Math.random() * 73)
+      password4.push(characters[n])
+    }
+    document.getElementById(`pass4`).innerHTML = `<p class="password-text">${password4.join('')}</p>`
+    console.log(password4)
 }
 
-generatePasswords()
+// generatePasswords()
